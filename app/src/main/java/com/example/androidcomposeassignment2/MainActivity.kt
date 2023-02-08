d930de0f35e81e2d41066dcd067bf31e94fcb179
Mike-415 <mgonz160@mail.ccsf.edu>
2023-02-08 13:50:27 -0800

    Added text

--- app/src/main/java/com/example/androidcomposeassignment2/MainActivity.kt
+++ app/src/main/java/com/example/androidcomposeassignment2/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -14,7 +15,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.androidcomposeassignment2.ui.theme.AndroidComposeAssignment2Theme
 
 class MainActivity : ComponentActivity() {
@@ -32,6 +38,7 @@ class MainActivity : ComponentActivity() {
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
                         TaskCompleteImage()
+                        TaskCompleteText(firstText = getString(R.string.first_text), secondText = getString(R.string.second_text))
                     }
                 }
             }
@@ -41,6 +48,26 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun TaskCompleteImage(){
     Image(painter = painterResource(id = R.drawable.img), contentDescription = null)
+}
+
+@Composable
+fun TaskCompleteText(firstText:String, secondText:String){
+    /*
+    Set the first Text composable to a Bold font weight, 24dp padding top, and 8dp padding bottom.
+    Set the second Text composable to a 16sp font size.
+     */
+    Text(
+        text = firstText,
+        fontWeight = FontWeight.Bold,
+        modifier = Modifier.padding(
+            top = 24.dp,
+            bottom = 8.dp
+        )
+    )
+    Text(
+        text = secondText,
+        fontSize = 16.sp
+    )
 }
 
 
